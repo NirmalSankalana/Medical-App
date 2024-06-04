8bb1886b80e24b0ce25f6d50c3b0a3c93d9e319f
_Dillusha_ <dilushamadushan9912@gmail.com>
2024-06-05 05:11:22 +0530

    Add app url into env

--- src/config/swaggerConfig.js
+++ src/config/swaggerConfig.js
@@ -21,7 +21,7 @@ const options = {
         }},
         servers: [
             {
-                url: 'http://localhost:3000/api',
+                url: URL,
                 description: 'Local server'
             },
         ],
